e2db3b3542af73675a487d2f6d1d5968cf2eb186
Ernst Reissner <reissner@simuline.eu>
2018-05-30 13:04:59 +0200

    @doc: all types supported.

--- javaoctave/src/main/java/eu/simuline/octave/io/impl/package-info.java
+++ javaoctave/src/main/java/eu/simuline/octave/io/impl/package-info.java
@@ -23,7 +23,552 @@
  * whereas the writer classes are in 
  * <code>META-INF/services/eu.simuline.octave.io.OctaveDataWriter</code>. 
 
- * Consequently, the classes are 
+ * Consequently, the classes are given by the following table 
+ * <table>
+ * <thead><tr align='left'>
+ *   <th>index</th>
+ *   <th>octave class</th>
+ *   <th>java class</th>
+ *   <th>reader</th>
+ *   <th>writer</th>
+ * </tr></thead>
+ * <tfoot align='left'>
+
+ *   <tr>
+ *   <th>1</th>
+ *   <th>&lt;unknown type></th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   </tr>
+
+ *   <tr>
+ *   <th>2</th>
+ *   <th>"cell"</th>
+ *   <th>{@link eu.simuline.octave.type.OctaveCell}</th>
+ *   <th>{@link eu.simuline.octave.io.impl.CellReader}</th>
+ *   <th>{@link eu.simuline.octave.io.impl.CellWriter}</th>
+ *   </tr>
+
+ *   <tr>
+ *   <th>3</th>
+ *   <th>"scalar"</th>
+ *   <th>see 5</th>
+ *   <th>{@link eu.simuline.octave.io.impl.ScalarReader}</th>
+ *   <th>see 5</th>
+ *   </tr>
+
+ *   <tr>
+ *   <th>4</th>
+ *   <th>"complex scalar"</th>
+ *   <th>see 7</th>
+ *   <th>{@link eu.simuline.octave.io.impl.ComplexScalarReader}</th>
+ *   <th>see 7</th>
+ *   </tr>
+
+ *   <tr>
+ *   <th>5</th>
+ *   <th>"matrix"</th>
+ *   <th>{@link eu.simuline.octave.type.OctaveDouble}</th>
+ *   <th>{@link eu.simuline.octave.io.impl.MatrixReader}</th>
+ *   <th>{@link eu.simuline.octave.io.impl.MatrixWriter}</th>
+ *   </tr>
+
+ *   <tr>
+ *   <th>6</th>
+ *   <th>"diagonal matrix"</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   </tr>
+
+ *   <tr>
+ *   <th>7</th>
+ *   <th>"complex matrix"</th>
+ *   <th>{@link eu.simuline.octave.type.OctaveComplex}</th>
+ *   <th>{@link eu.simuline.octave.io.impl.ComplexMatrixReader}</th>
+ *   <th>???</th>
+ *   </tr>
+
+ *   <tr>
+ *   <th>8</th>
+ *   <th>"complex diagonal matrix"</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   </tr>
+
+ *   <tr>
+ *   <th>9</th>
+ *   <th>"range"</th>
+ *   <th>OctaveFake</th>
+ *   <th>FakeRangeReader</th>
+ *   <th>FakeWriter</th>
+ *   </tr>
+
+ *   <tr>
+ *   <th>10</th>
+ *   <th>"bool"</th>
+ *   <th>---</th>
+ *   <th>{@link eu.simuline.octave.io.impl.BooleanSingleReader}</th>
+ *   <th>---</th>
+ *   </tr>
+
+ *   <tr>
+ *   <th>11</th>
+ *   <th>"bool matrix"</th>
+ *   <th>{@link eu.simuline.octave.type.OctaveBoolean}</th>
+ *   <th>{@link eu.simuline.octave.io.impl.BooleanReader}</th>
+ *   <th>{@link eu.simuline.octave.io.impl.BooleanWriter}</th> 
+ *   </tr>
+
+ *   <tr>
+ *   <th>12</th>
+ *   <th>"string"</th>
+ *   <th>{@link eu.simuline.octave.type.OctaveString}</th>
+ *   <th>{@link eu.simuline.octave.io.impl.OctaveStringReader}</th>
+ *   <th>{@link eu.simuline.octave.io.impl.OctaveStringWriter}/---</th>
+ *   </tr>
+
+ *   <tr>
+ *   <th>13</th>
+ *   <th>"sq_string"</th>
+ *   <th>see 12</th>
+ *   <th>{@link eu.simuline.octave.io.impl.OctaveSqStringReader}</th>
+ *   <th>{@link eu.simuline.octave.io.impl.OctaveStringWriter}/---</th>
+ *   </tr>
+
+ *   <tr>
+ *   <th>14</th>
+ *   <th>"int8 scalar"</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   </tr>
+
+ *   <tr>
+ *   <th>15</th>
+ *   <th>"int16 scalar"</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   </tr>
+
+ *   <tr>
+ *   <th>16</th>
+ *   <th>"int32 scalar"</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   </tr>
+
+ *   <tr>
+ *   <th>17</th>
+ *   <th>"int64 scalar"</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   </tr>
+
+ *   <tr>
+ *   <th>18</th>
+ *   <th>"uint8 scalar"</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   </tr>
+
+ *   <tr>
+ *   <th>19</th>
+ *   <th>"uint16 scalar"</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   </tr>
+
+ *   <tr>
+ *   <th>20</th>
+ *   <th>"uint32 scalar"</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   </tr>
+
+ *   <tr>
+ *   <th>21</th>
+ *   <th>"uint64 scalar"</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   </tr>
+
+
+
+ *   <tr>
+ *   <th>22</th>
+ *   <th>"int8 matrix"</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   </tr>
+
+ *   <tr>
+ *   <th>23</th>
+ *   <th>"int16 matrix"</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   </tr>
+
+ *   <tr>
+ *   <th>24</th>
+ *   <th>"int32 matrix"</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   </tr>
+
+ *   <tr>
+ *   <th>25</th>
+ *   <th>"int64 matrix"</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   </tr>
+
+ *   <tr>
+ *   <th>26</th>
+ *   <th>"uint8 matrix"</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   </tr>
+
+ *   <tr>
+ *   <th>26</th>
+ *   <th>"uint8 matrix" Uint??</th>
+ *   <th>{@link eu.simuline.octave.type.OctaveInt}</th>
+ *   <th>{@link eu.simuline.octave.io.impl.Uint8MatrixReader}</th>
+ *   <th>---</th>
+ *   </tr>
+
+ *   <tr>
+ *   <th>27</th>
+ *   <th>"uint16 matrix"</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   </tr>
+
+ *   <tr>
+ *   <th>28</th>
+ *   <th>"uint32 matrix"</th>
+ *   <th>{@link eu.simuline.octave.type.OctaveInt}</th>
+ *   <th>{@link eu.simuline.octave.io.impl.Int32MatrixReader}</th>
+ *   <th>{@link eu.simuline.octave.io.impl.Int32MatrixWriter}</th>
+ *   </tr>
+
+ *   <tr>
+ *   <th>29</th>
+ *   <th>"uint64 matrix"</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   </tr>
+
+
+
+
+ *   <tr>
+ *   <th>30</th>
+ *   <th>"sparse bool matrix"</th>
+ *   <th>{@link eu.simuline.octave.type.OctaveSparseBoolean}</th>
+ *   <th>{@link eu.simuline.octave.io.impl.SparseBooleanReader}</th>
+ *   <th>{@link eu.simuline.octave.io.impl.SparseBooleanWriter}</th>
+ *   </tr>
+
+ *   <tr>
+ *   <th>31</th>
+ *   <th>"sparse matrix"</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   </tr>
+
+ *   <tr>
+ *   <th>32</th>
+ *   <th>"sparse complex matrix"</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   </tr>
+
+ *   <tr>
+ *   <th>33</th>
+ *   <th>"struct"</th>
+ *   <th>{@link eu.simuline.octave.type.OctaveStruct}</th>
+ *   <th>{@link eu.simuline.octave.io.impl.StructReader}</th>
+ *   <th>{@link eu.simuline.octave.io.impl.StructWriter}</th>
+ *   </tr>
+
+ *   <tr>
+ *   <th>34</th>
+ *   <th>"scalar struct"</th>
+ *   <th>{@link eu.simuline.octave.type.OctaveStruct}</th>
+ *   <th>{@link eu.simuline.octave.io.impl.ScalarStructReader}</th>
+ *   <th>---</th>
+ *   </tr>
+
+ *   <tr>
+ *   <th>35</th>
+ *   <th>"class"</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   </tr>
+
+ *   <tr>
+ *   <th>36</th>
+ *   <th>"cs-list"</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   </tr>
+
+ *   <tr>
+ *   <th>37</th>
+ *   <th>"magic-colon"</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   </tr>
+
+ *   <tr>
+ *   <th>38</th>
+ *   <th>"built-in function"</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   </tr>
+
+ *   <tr>
+ *   <th>39</th>
+ *   <th>"user-defined function"</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   </tr>
+
+ *   <tr>
+ *   <th>40</th>
+ *   <th>"dynamically-linked function"</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   </tr>
+
+ *   <tr>
+ *   <th>41</th>
+ *   <th>"function handle"</th>
+ *   <th>{@link eu.simuline.octave.type.OctaveFunctionHandle}</th>
+ *   <th>????</th>
+ *   <th>{@link eu.simuline.octave.io.impl.FunctionHandleWriter}</th>
+ *   </tr>
+
+ *   <tr>
+ *   <th>42</th>
+ *   <th>"inline function" (which will be deprecated)</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   </tr>
+
+
+ *   <tr>
+ *   <th>43</th>
+ *   <th>"float scalar"</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   </tr>
+
+ *   <tr>
+ *   <th>44</th>
+ *   <th>"float complex scalar"</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   </tr>
+
+ *   <tr>
+ *   <th>45</th>
+ *   <th>"float matrix"</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   </tr>
+
+ *   <tr>
+ *   <th>46</th>
+ *   <th>"float diagonal matrix"</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   </tr>
+
+ *   <tr>
+ *   <th>47</th>
+ *   <th>"float complex matrix"</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   </tr>
+
+ *   <tr>
+ *   <th>48</th>
+ *   <th>"float complex diagonal matrix"</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   </tr>
+
+
+
+
+ 
+ *   <tr>
+ *   <th>49</th>
+ *   <th>"permutation matrix"</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   </tr>
+
+ *   <tr>
+ *   <th>50</th>
+ *   <th>"null_matrix"</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   </tr>
+
+ *   <tr>
+ *   <th>51</th>
+ *   <th>"null_string"</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   </tr>
+
+ *   <tr>
+ *   <th>52</th>
+ *   <th>"null_sq-string"</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   </tr>
+
+ *   <tr>
+ *   <th>53</th>
+ *   <th>"lazy_index"</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   </tr>
+
+ *   <tr>
+ *   <th>54</th>
+ *   <th>"onCleanup"</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   </tr>
+
+ *   <tr>
+ *   <th>55</th>
+ *   <th>"octave_java"</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   </tr>
+
+ *   <tr>
+ *   <th>56</th>
+ *   <th>"object"</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   <th>---</th>
+ *   </tr>
+
+
+
+ * </tfoot>
+ * </table>
+ * does not take fake into account (see below). 
+ * does not take integer types into account (see below). 
+
+ * <p>
+ * There three categories of basic types in octave: 
+ * boolean, floating point and integer. 
+ * <ul>
+ * <li>
+ * <code>bool</code> is the boolean (scalar) type, 
+ * <li>
+ * <code>double</code> and <code>float</code> 
+ * are the floating point (scalar) types, 
+ * <li>
+ * <code>int8</code>, <code>int16</code>, <code>int32</code> 
+ * and <code>int64</code>, are the signed (scalar) integer types and 
+ * <code>uint8</code>, <code>uint16</code>, <code>uint32</code> 
+ * and <code>uint64</code>, are the according unsigned (scalar) integer types. 
+ * </ul>
+ * <p>
+ * Principally, all these types may be vector valued, matrix valued 
+ * or even in higher dimensions. 
+ * To express this, they are endowed with the suffix <code>matrix</code> 
+ * even for higher dimensions 
+ * and are thus called <code>bool matrix</code>, <code>float matrix</code>, 
+ * <code>int8 matrix</code>, <code>int16 matrix</code>, 
+ * <code>int32 matrix</code> and <code>int64 matrix</code> 
+ * and accordingly for the unsigned integers. 
+ * Since double is the standard type occurring most frequently,
+ * what should be called <code>double matrix</code> 
+ * is just called <code>matrix</code>. 
+ * <p>
+ * Likewise, if endowed with the suffix <code>scalar</code>, 
+ * only scalars, i.e. 1x1 matrices are allowed. 
+ * This type is to spare memory, i.e. the format information. 
+ * likewise, 
+ * <code>double scalar</code> is just called <code>scalar</code> and 
+ * <code>bool scalar</code> is just called <code>bool</code>, 
+ * which seems strange but comes from the fact, 
+ * that bools mostly come as scalars. 
+ * The other names are regular, 
+ * as <code>int8 scalar</code>, <code>int16 scalar</code>, 
+ * <code>int32 scalar</code> and <code>int64 scalar</code>. 
+ * Although strutcs are not atomic but as the name says structures, 
+ * they can in general be array valued. 
+ * If not, it is a <code>scalar struct</code>, 
+ * in the general case it is just a <code>struct</code> 
+ * Note that this is the only case where the text <code>scalar </code> 
+ * is prefixed (since <code>scalar</code> itself 
+ * is short for <code>double scalar</code>). 
+ * <p>
+ * For the moment, we do not treat complex types. 
+ * <p>
+ * Floating matrix types may be diagonal. 
+ * Thus we have <code>double diagonal matrix</code> 
+ * called just <code>diagonal matrix</code>, 
+ * <code>float diagonal matrix</code>. 
+ * <p>
+ * Floating matrix types and also boolean matrices may be sparse. 
+ * Thus we have <code>double sparse matrix</code> 
+ * called just <code>sparse matrix</code>, 
+ * but no <code>float sparse matrix</code>. 
+
+
+
+
  * <ul>
  * <li>
  * {@link eu.simuline.octave.io.impl.BooleanReader}/
@@ -113,7 +658,7 @@
  * <li>
  * {@link eu.simuline.octave.io.impl.SparseBooleanReader}/ 
  * {@link eu.simuline.octave.io.impl.SparseBooleanWriter} 
- * for reading/writing octave (double) "sparse bool matrix"s 
+ * for reading/writing octave "sparse bool matrix"s 
  * represented by {@link eu.simuline.octave.type.OctaveSparseBoolean}. 
  * <p>
  * Note that there is no treatment for the octave types "sparse matrix" 
