53a08507da7ad8b7e6096f78e33babce0c7f20bc
Ernst Reissner <ernst.reissner@simuline.eu>
2020-12-01 13:08:04 +0100

    Eliminated TestOctaveUtils.java moving all tests to TestMetaInfo

--- javaoctave/src/test/java/eu/simuline/octave/TestMetaInfo.java
+++ javaoctave/src/test/java/eu/simuline/octave/TestMetaInfo.java
@@ -15,12 +15,10 @@
  */
 package eu.simuline.octave;
 
-
-import static org.junit.Assert.assertTrue;
-
 import java.util.Collection;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 //import org.junit.Ignore;
 import org.junit.Test;
@@ -78,4 +76,34 @@ public class TestMetaInfo {
 	Collection<String> names = octave.getNamesOfPackagesInstalled();
 	assertTrue(names.contains("quaternion"));
     }
+
+    /**
+     * Test that the list of variables is empty. 
+     */
+    @Test public void testListVarsEmpty() {
+        final OctaveEngine octave = new OctaveEngineFactory().getScriptEngine();
+        final Collection<String> collection = octave.listVars();
+        assertEquals(collection.toString(), 0, collection.size());
+        octave.close();
+    }
+
+    /**
+     * Test that the list of variables has 1 or 2 entries. 
+     * **** better: test concrete collection of variables. 
+     */
+    @Test public void testListVarsOneTwo() {
+        final OctaveEngine octave = new OctaveEngineFactory().getScriptEngine();
+
+        octave.eval("my_var = 42;");
+        final Collection<String> collection1 = octave.listVars();
+        assertEquals(collection1.toString(), 1, collection1.size());
+
+        octave.eval("1 + 2;");
+        octave.eval("my_other_var = 42;");
+        final Collection<String> collection2 = octave.listVars();
+        assertEquals(collection2.toString(), 2, collection2.size());
+
+        octave.close();
+    }
+
 }
